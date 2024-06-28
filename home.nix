3c41905eaa69a1e2d6c690d1c9a2c16267c857f3
ruben <ruben@sdu.as>
2024-06-28 13:56:29 +0200

    use direnv

--- home.nix
+++ home.nix
@@ -170,6 +170,10 @@
     };
   };
 
+  programs.direnv = {
+	enable = true;
+  };
+
   # This value determines the home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new home Manager release introduces backwards
