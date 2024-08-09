c1a48ba4ab82136297413e8d321cdf5dcc8fde18
Ruben Beck <ruben.beck@pm.me>
2024-08-09 09:15:32 +0200

    use home-manager for sway-bar

--- home/desktop/sway-bars.nix
+++ home/desktop/sway-bars.nix
@@ -1,3 +1,55 @@
-{...}: {
-  programs.i3status-rust.enable = true;
+{config, ...}: let
+  id = "bottom";
+in {
+  programs.i3status-rust = {
+    enable = true;
+    bars = {
+      "${id}" = {
+        icons = "awesome6";
+        blocks = [
+          {
+            block = "sound";
+            format = "$output_description $icon {$volume.eng(w:2) |}";
+          }
+          {
+            block = "cpu";
+          }
+          {block = "memory";}
+          {block = "disk_space";}
+          {
+            block = "time";
+            format = "$icon $timestamp.datetime(f:'%F %a %R')";
+          }
+          {
+            block = "keyboard_layout";
+            driver = "sway";
+            mappings = {
+              "Norsk (NO)" = "NO";
+              "English (US)" = "US";
+            };
+          }
+          {
+            block = "sound";
+            device_kind = "source";
+            format = "$icon";
+          }
+          {
+            block = "battery";
+            missing_format = "";
+          }
+        ];
+      };
+    };
+  };
+  wayland.windowManager.sway.config.bars = [
+    {
+      statusCommand = "i3status-rs ${config.xdg.configHome}/i3status-rust/config-${id}.toml";
+      trayOutput = "none";
+      fonts = {
+        names = ["monospace"];
+        size = 12.0;
+        style = "Bold";
+      };
+    }
+  ];
 }
