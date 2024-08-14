e7a82e8fa0986c79cd5d64b64bb754c0044d120c
Ruben Beck <ruben.beck@pm.me>
2024-08-14 16:34:50 +0200

    add yubikey functionality

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -14,6 +14,7 @@
     ./sway.nix
     ./programs
     ./zerotier.nix
+    ./yubikey.nix
   ];
 
   security.doas.enable = true;
@@ -237,10 +238,10 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
   # List services that you want to enable:
 
