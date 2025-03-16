1c095f87f10085a1ab1d94d810370159546ec7bf
Ruben Beck <ruben.beck@pm.me>
2025-03-16 14:12:22 +0100

    add zerotier network|

--- nixos/zerotier.nix
+++ nixos/zerotier.nix
@@ -2,6 +2,9 @@
 {
   services.zerotierone = {
     enable = true;
-    joinNetworks = [ "e5cd7a9e1ccee855" ];
+    joinNetworks = [
+      "e5cd7a9e1ccee855"
+      "60ee7c034a32e1b9"
+    ];
   };
 }
