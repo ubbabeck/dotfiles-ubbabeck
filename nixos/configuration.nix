335052d6b1d329a47ab673830b2974f9df4f18f8
Ruben Beck <ruben.beck@pm.me>
2025-05-29 09:00:05 +0200

    disable tlp

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -202,23 +202,6 @@
   # ----- Podman ---------
 
   powerManagement.enable = true;
-  services.tlp.enable = true;
-  # services.tlp.settings = {
-  #   CPU_SCALING_GOVERNOR_ON_AC = "performance";
-  #   CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-  #   CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-  #   CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-
-  #   CPU_MIN_PERF_ON_AC = 0;
-  #   CPU_MAX_PERF_ON_AC = 100;
-  #   CPU_MIN_PERF_ON_BAT = 0;
-  #   CPU_MAX_PERF_ON_BAT = 20;
-
-  #   #Optional helps save long term battery health
-  #   #START_CHARGE_THRESH_BAT0 = 40; # 30 and bellow it starts to charge
-  #   #STOP_CHARGE_THRESH_BAT0 = 100; # 79 and above it stops charging
-  # };
   # Configure keymap in wayland
   services.xserver = {
     xkb = {
