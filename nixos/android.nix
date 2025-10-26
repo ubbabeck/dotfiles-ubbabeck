8d0c581c7128bf0c1833c7e5e8df36ec059fdae6
Ruben Beck <ruben.beck@pm.me>
2025-10-26 13:13:42 +0100

    fix android config

--- nixos/android.nix
+++ nixos/android.nix
@@ -1,11 +1,6 @@
-{ pkgs, ... }:
+{ ... }:
 {
 
   programs.adb.enable = true;
   users.users.ruben.extraGroups = [ "adbusers" ];
-
-  services.udev.packages = [
-    pkgs.android-udev-rules
-  ];
-
 }
