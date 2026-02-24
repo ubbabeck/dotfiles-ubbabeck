2fa2bd7fd52cfc59a894ff300d088f8ea33cd394
Ruben Beck <ruben.beck@pm.me>
2026-02-24 11:27:50 +0100

    needed for default network access

--- nixosModules/libvirt.nix
+++ nixosModules/libvirt.nix
@@ -1,6 +1,7 @@
 {
   virtualisation.libvirtd.enable = true;
   users.users.ruben.extraGroups = [ "libvirtd" ];
+  networking.firewall.trustedInterfaces = [ "virbr0" ];
   networking.firewall.checkReversePath = false;
   programs.virt-manager.enable = true;
 }
