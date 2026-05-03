1d9901322e0652d72b635ef497f5eb0603f92a22
Ruben Beck <ruben.beck@pm.me>
2026-05-03 08:03:34 +0000

    refactor neovim

--- home-manager/modules/neovim/flake-module.nix
+++ home-manager/modules/neovim/flake-module.nix
@@ -3,71 +3,74 @@
   perSystem =
     {
       pkgs,
-      config,
       ...
     }:
+    let
+      nvim-lsp-packages = with pkgs; [
+        nodejs # copilot
+        # tree-sitter-cli needs a C compiler to build parsers from source
+        (pkgs.symlinkJoin {
+          name = "tree-sitter-wrapped";
+          paths = [ pkgs.tree-sitter ];
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/tree-sitter \
+              --prefix PATH : ${lib.makeBinPath [ pkgs.stdenv.cc ]}
+          '';
+        })
+
+        # based on ./suggested-pkgs.json
+        basedpyright
+        bash-language-server
+        clang-tools
+        golangci-lint
+        gopls
+        lua-language-server
+        nil
+        nixd
+        nixfmt
+        deadnix
+        statix
+        prettierd
+        ruff
+        selene
+        shellcheck
+        shfmt
+        stylua
+        vtsls
+        yaml-language-server
+        vscode-langservers-extracted
+        # based on https://github.com/ray-x/go.nvim#go-binaries-install-and-update
+        go
+        delve
+        ginkgo
+        gofumpt
+        golines
+        gomodifytags
+        gotests
+        gotestsum
+        (lib.lowPrio gotools)
+        govulncheck
+        iferr
+        impl
+
+        # others
+        rust-analyzer
+        clippy
+        rustfmt
+
+        zls
+        terraform-ls
+        taplo
+        typos
+        typos-lsp
+      ];
+    in
     {
       legacyPackages = {
-        nvim-lsp-packages = with pkgs; [
-          nodejs # copilot
-
-          # tree-sitter-cli needs a C compiler to build parsers from source
-          (pkgs.symlinkJoin {
-            name = "tree-sitter-wrapped";
-            paths = [ pkgs.tree-sitter ];
-            nativeBuildInputs = [ pkgs.makeWrapper ];
-            postBuild = ''
-              wrapProgram $out/bin/tree-sitter \
-                --prefix PATH : ${lib.makeBinPath [ pkgs.stdenv.cc ]}
-            '';
-          })
-
-          # based on ./suggested-pkgs.json
-          basedpyright
-          bash-language-server
-          clang-tools
-          golangci-lint
-          lua-language-server
-          nil
-          nixd
-          nixfmt
-          deadnix
-          statix
-          prettierd
-          ruff
-          selene
-          shellcheck
-          shfmt
-          stylua
-          vtsls
-          yaml-language-server
-          vscode-langservers-extracted
-          # based on https://github.com/ray-x/go.nvim#go-binaries-install-and-update
-          go
-          delve
-          ginkgo
-          gofumpt
-          golines
-          gomodifytags
-          gotests
-          gotestsum
-          gotools
-          govulncheck
-          iferr
-          impl
-
-          # others
-          rust-analyzer
-          clippy
-          rustfmt
-
-          zls
-          terraform-ls
-          taplo
-          typos
-          typos-lsp
-        ];
+        inherit nvim-lsp-packages;
       };
+
       packages = {
         neovim = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped {
           wrapRc = false;
@@ -77,7 +80,7 @@
 
         nvim = pkgs.callPackage ./nvim-standalone.nix {
           nvim-appname = "nvim-ubbabeck";
-          inherit (config.legacyPackages) nvim-lsp-packages;
+          inherit nvim-lsp-packages;
         };
       };
     };
