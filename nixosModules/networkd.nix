5382575cd1dd5c4feae65e09430ca9f6a110eb0e
Ruben Beck <ruben.beck@pm.me>
2026-05-05 08:44:37 +0000

    add networkd

---
+++ nixosModules/networkd.nix
@@ -0,0 +1,10 @@
+{
+  systemd.network.enable = true;
+
+  # don't take down the network for too long
+  systemd.services.systemd-networkd.stopIfChanged = false;
+  # Services that are only restarted might be not able to resolve when this is stopped before
+  systemd.services.systemd-resolved.stopIfChanged = false;
+
+  networking.dhcpcd.enable = false;
+}
